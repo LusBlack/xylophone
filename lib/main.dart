17e3533ba2c427ada8d5839da3f23bd8ea2c3c93
Louis <Lou.ikwuegbu@gmail.com>
2025-09-17 23:13:02 +0100

    added multiple signs and added further color properties and positions

--- lib/main.dart
+++ lib/main.dart
@@ -9,15 +9,112 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child:  Center(
-            child: TextButton(
-              onPressed: () {
-                final player = AudioPlayer();
-                 player.play(AssetSource('note1.wav'));
-              },
-              child: const Text("Play Sound"),
+          //child:  Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Container(
+                  color: Colors.red,
+                  child: TextButton(
+                    onPressed: () {
+                      final player = AudioPlayer();
+                       player.play(AssetSource('note1.wav'));
+                    },
+                    child: const Text(""),
+                  ),
+                ),
+                // SizedBox(
+                //   height: 10.0,
+                // ),
+                //sound 2
+                Container(
+                  color: Colors.deepOrangeAccent,
+                  child: TextButton(
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note2.wav'));
+                  },
+                  child: const Text(""),
+                                 ),
+                ),
+
+                //  SizedBox(
+                //   height: 10.0,
+                // ),
+                //sound 3
+                Container(
+                  color: Colors.yellow,
+                  child: Expanded(
+                      child: TextButton(
+                        onPressed: () {
+                          final player = AudioPlayer();
+                          player.play(AssetSource('note3.wav'));
+                        },
+                    child: const Text(""),
+                    ),
+                  )
+                ),
+                // SizedBox(
+                //   height: 10.0,
+                // ),
+                 //sound 4
+                Container(
+                  color: Colors.green,
+                  child: TextButton(
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note4.wav'));
+                  },
+                  child: const Text(""),
+                  ),
+                ),
+                // SizedBox(
+                //   height: 10.0,
+                // ),
+                 //sound 5
+                Container(
+                  color: Colors.teal,
+                  child: TextButton(
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note5.wav'));
+                  },
+                  child: const Text(""),
+                  ),
+                ),
+                // SizedBox(
+                //   height: 10.0,
+                // ),
+                 //sound 6
+                Container(
+                  color: Colors.blue,
+                  child: TextButton(
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note6.wav'));
+                  },
+                  child: const Text(""),
+                  ),
+                ),
+                // SizedBox(
+                //   height: 10.0,
+                // ),
+                 //sound 7
+                 Container(
+                  color: Colors.purple,
+                  child: TextButton(
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note7.wav'));
+                  },
+                  child: const Text(""),
+                  ),
+                ),
+                
+              ],
             ),
-          ),
+          //),
+          
         ),
       ),
     );
