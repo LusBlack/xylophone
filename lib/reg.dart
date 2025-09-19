1baae74d96da3ca73e994746870e0d6db26084fd
Louis <Lou.ikwuegbu@gmail.com>
2025-09-19 01:05:10 +0100

    refactored into buildkeyfunction

---
+++ lib/reg.dart
@@ -0,0 +1,22 @@
+void main() {
+  int step1Result = add(n1: 5, n2: 9);
+
+  int step2Result = multiply(step1Result, 5);
+
+  double finalResult = step2Result / 3;
+
+print(finalResult);
+
+}
+ 
+int add({ n1,  n2}) {
+  return n1 + n2;
+}
+
+int multiply(num1, num2) {
+ return num1 * num2;
+}
+
+
+
+
